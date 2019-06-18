d7202eabf1912e958b4daa0aff359b21da853336
snehasosa <manjunathkankatteko@gmail.com>
2019-06-18 15:41:44 +0530

    Updates

---
+++ precache-manifest.ec3b06a7ac82224c706c77fd00318bae.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbea9fb16c9564d176ac5d00c8a3e5d3",
+    "url": "/org/index.html"
+  },
+  {
+    "revision": "edd4237f9d2a57c7a21c",
+    "url": "/org/static/css/2.2e0f3f0b.chunk.css"
+  },
+  {
+    "revision": "482811d4a116f16cdebd",
+    "url": "/org/static/css/main.118f00af.chunk.css"
+  },
+  {
+    "revision": "edd4237f9d2a57c7a21c",
+    "url": "/org/static/js/2.e2560621.chunk.js"
+  },
+  {
+    "revision": "482811d4a116f16cdebd",
+    "url": "/org/static/js/main.104cc4e8.chunk.js"
+  },
+  {
+    "revision": "41ca97bc34318558503c",
+    "url": "/org/static/js/runtime~main.cde13fc3.js"
+  },
+  {
+    "revision": "21159dacdae91691ba6259006e7294d4",
+    "url": "/org/static/media/slide1.21159dac.jpg"
+  },
+  {
+    "revision": "6a6f1c35b639c9696428019ad2c1efd9",
+    "url": "/org/static/media/slide2.6a6f1c35.jpg"
+  },
+  {
+    "revision": "bd3190b5361dbc2cb43a8ee19bbe8a19",
+    "url": "/org/static/media/slide3.bd3190b5.jpg"
+  },
+  {
+    "revision": "fed61ff7da6ad685e909b22de7839992",
+    "url": "/org/static/media/slide4.fed61ff7.jpg"
+  },
+  {
+    "revision": "f19d81dce3e2a576b4affdc98ed340b3",
+    "url": "/org/static/media/slide5.f19d81dc.jpg"
+  },
+  {
+    "revision": "8ab30cef4af604c2b1e071c1fc77eda2",
+    "url": "/org/static/media/slide6.8ab30cef.jpg"
+  },
+  {
+    "revision": "701b936a858c5c43b4a761fd223ae804",
+    "url": "/org/static/media/sneha_logo.701b936a.jpg"
+  },
+  {
+    "revision": "8d8e3a567b3c8cb7fb24fb3f1d945e0f",
+    "url": "/org/static/media/sosa_logo.8d8e3a56.jpg"
+  }
+]);
